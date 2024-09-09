4f35275ab73651cc1a5de6ac0d6042d4491df9cd
Nikhil Joshi <70149907+clasher3531@users.noreply.github.com>
2024-09-09 16:26:09 +0530

    Update Map.js

--- Map.js
+++ Map.js
@@ -1,21 +1,32 @@
 // polyfil for an array.map function
 
-Array.prototype.myMap = function(callback) {
+var obj = {
+    a: 1,
+    divideByThree: function(item) {
+        return ((item*3) + this.a);
+    }
+}
+
+Array.prototype.myMap = function(callback, thisArgs) {
+    if (typeof callback !== 'function') {
+        throw new Error('Callback as function required');
+    }
     try {
         var contextArray = this;
         var result = [];
+        var bindWithThis = callback.bind(thisArgs);
         for (var i = 0; i < contextArray.length; i++) {
             var element = contextArray[i];
-            result.push(callback(element, i, this));
+            result.push(bindWithThis(element, i, contextArray));
         }
-    return result;
+        return result;
     } catch(e) {
         return "Error"
     }
 };
 
+
+
 var a = [1, 2, 3, 4, 5, 6];
-var b = a.myMap(function(item) {
-    return item * 2;
-});
-console.log(b); // [2, 4, 6, 8, 10, 12]
+var b = a.myMap(obj.divideByThree, obj);
+console.log(b); // [ 4, 7, 10, 13, 16, 19 ]
