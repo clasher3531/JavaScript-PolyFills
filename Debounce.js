ab5bda2fe2bff861b9b13758b0a4659b3803dd93
Nikhil Joshi <70149907+clasher3531@users.noreply.github.com>
2024-09-18 15:58:07 +0530

    Update Debounce.js

--- Debounce.js
+++ Debounce.js
@@ -1,18 +1,22 @@
 let counter = 0;
-function getData() {
-    console.log('Fetching some data......... ' + counter++);
+let inputElement = document.getElementById('debounce_input');
+function getData(arg1, arg2) {
+    var request = {
+        counter: ++counter,
+        data: this.value,
+        arg1: arg1,
+        arg2: arg2
+    }
+    console.log('Fetching data .......... ');
+    console.log('request', request);
 }
-
-function debounce(fn, delay) {
-    let timer;
+const debounce = function(callback, delay, ...args) {
+    let timeoutId;
     return function() {
-        let context = this,
-            args = arguments;
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            fn.apply(context, args)
-        }, delay);
+        clearTimeout(timeoutId);
+        let bindCallBackWithThis = callback.bind(this, ...args);
+        timeoutId = setTimeout(bindCallBackWithThis, delay);
     }
 }
 
-const improveFunction = debounce(getData, 300);
+inputElement.addEventListener('input', debounce(getData, 300, 'arg1', 'arg2'))
