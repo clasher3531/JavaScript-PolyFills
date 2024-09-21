245d5e968aee343c9ae422c1e936b7b3391e3548
clasher3531 <nikhil.a.joshi@capgemini.com>
2024-09-21 15:26:11 +0530

    Update Call Polyfill

--- Function/Call-polyfill.js
+++ Function/Call-polyfill.js
@@ -1,38 +1,41 @@
 let personName = {
-    firstName: "Sachin",
-    lastName: "Tendulkar",
-    getNameAndState: function(state, country) {
-        console.log(this.firstName + " " + this.lastName + " from " + state + " in " + country);
-    }
-}
+  firstName: "Sachin",
+  lastName: "Tendulkar",
+  getNameAndState: function (state, country) {
+    console.log(
+      this.firstName + " " + this.lastName + " from " + state + " in " + country
+    );
+  },
+};
 
-const getFullName =  function(state, country) {
-    console.log(this.firstName + " " + this.lastName + " from " + state + " in " + country);
-}
+const getFullName = function (state, country) {
+  console.log(
+    this.firstName + " " + this.lastName + " from " + state + " in " + country
+  );
+};
 
-var personName2  = {
-    firstName: "Rahul",
-    lastName: "Dravid"
-}
+var personName2 = {
+  firstName: "Rahul",
+  lastName: "Dravid",
+};
 
 // pollyfill for call method
 
-Function.prototype.myCall = function(context, ...args) {
-    try {
-        let currentContext = context || globalThis;
-        let randomKey = Math.random();
-        while(currentContext[randomKey] !== undefined) {
-            randomKey = Math.random();
-        }
-        currentContext[randomKey] = this;
-        var result = currentContext[randomKey](...args);
-        delete currentContext[randomKey];
-        return result;
-    } catch(e) {
-        console.error('Syntax Error');
+Function.prototype.myCall = function (context, ...args) {
+  try {
+    let currentContext = context || globalThis;
+    if (typeof currentContext !== "object") {
+      return new Object(context);
     }
-    
-}
+    let randomKey = Symbol();
+    currentContext[randomKey] = this;
+    let result = currentContext[randomKey](...args);
+    delete currentContext[randomKey];
+    return result;
+  } catch (e) {
+    console.error("Syntax Error");
+  }
+};
 
-personName.getNameAndState.myCall(personName2, "Karnataka", "India")
-getFullName.myCall(personName, 'Maharashtra', "India");
+personName.getNameAndState.myCall(personName2, "Karnataka", "India");
+getFullName.myCall(personName, "Maharashtra", "India");
