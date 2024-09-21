71e06ebdcee4be172ef019ba1600cb258fe63fb5
clasher3531 <nikhil.a.joshi@capgemini.com>
2024-09-21 15:32:16 +0530

    update Apply polyfill

--- Function/Apply-PolyFill.js
+++ Function/Apply-PolyFill.js
@@ -1,37 +1,41 @@
 // pollyfil for apply method
 
 let personName = {
-    firstName: "Sachin",
-    lastName: "Tendulkar",
-    getNameAndState: function(state, country) {
-        console.log(this.firstName + " " + this.lastName + " from " + state + " in " + country);
+  firstName: "Sachin",
+  lastName: "Tendulkar",
+  getNameAndState: function (state, country) {
+    console.log(
+      this.firstName + " " + this.lastName + " from " + state + " in " + country
+    );
+  },
+};
+
+const getFullName = function (state, country) {
+  console.log(
+    this.firstName + " " + this.lastName + " from " + state + " in " + country
+  );
+};
+
+var personName2 = {
+  firstName: "Rahul",
+  lastName: "Dravid",
+};
+
+Function.prototype.myApply = function (context, args) {
+  try {
+    let currentContext = context || globalThis;
+    if (typeof currentContext !== "object") {
+      return new Object(context);
     }
-}
-
-const getFullName =  function(state, country) {
-    console.log(this.firstName + " " + this.lastName + " from " + state + " in " + country);
-}
-
-var personName2  = {
-    firstName: "Rahul",
-    lastName: "Dravid"
-}
-
-Function.prototype.myApply = function(context, args) {
-    try {
-        let currentContext = context || globalThis;
-        let randomKey = Math.random();
-        while(currentContext[randomKey] !== undefined) {
-            randomKey = Math.random();
-        }
-        currentContext[randomKey] = this;
-        let result = currentContext[randomKey](...args);
-        delete currentContext[randomKey];
-        return result;
-    } catch(e) {
-        console.error('Syntax Error');
-    }
-}
+    let randomKey = Symbol();
+    currentContext[randomKey] = this;
+    let result = currentContext[randomKey](...args);
+    delete currentContext[randomKey];
+    return result;
+  } catch (e) {
+    console.error("Syntax Error");
+  }
+};
 
 personName.getNameAndState.myApply(personName2, ["Karnataka", "India"]);
 getFullName.myApply(personName, ["Maharashtra", "India"]);
