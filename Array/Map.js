eaa5b6e733686faf6c18b30e62beec8554e43a59
clasher3531 <nikhil.a.joshi@capgemini.com>
2024-09-21 16:00:02 +0530

    Update Map Polyfill

--- Array/Map.js
+++ Array/Map.js
@@ -1,31 +1,32 @@
 // polyfil for an array.map function
 
 var obj = {
-    a: 1,
-    divideByThree: function(item) {
-        return ((item*3) + this.a);
-    }
-}
-
-Array.prototype.myMap = function(callback, thisArgs) {
-    if (typeof callback !== 'function') {
-        throw new Error('Callback as function required');
-    }
-    try {
-        var contextArray = this;
-        var result = [];
-        var bindWithThis = callback.bind(thisArgs);
-        for (var i = 0; i < contextArray.length; i++) {
-            var element = contextArray[i];
-            result.push(bindWithThis(element, i, contextArray));
-        }
-        return result;
-    } catch(e) {
-        return "Error"
-    }
+  a: 1,
+  divideByThree: function (item) {
+    return item * 3 + this.a;
+  },
 };
 
-
+Array.prototype.myMap = function (callback, thisArg) {
+  if (typeof callback !== "function") {
+    throw new Error("Callback as function required");
+  }
+  try {
+    var contextArray = this;
+    var arrayLength = this.length;
+    var result = new Array(arrayLength);
+    var bindWithThis = callback.bind(thisArg);
+    for (var i = 0; i < arrayLength; i++) {
+      var element = contextArray[i];
+      if (i in this) {
+        result[i] = bindWithThis(element, i, contextArray);
+      }
+    }
+    return result;
+  } catch (e) {
+    return "Error";
+  }
+};
 
 var a = [1, 2, 3, 4, 5, 6];
 var b = a.myMap(obj.divideByThree, obj);
