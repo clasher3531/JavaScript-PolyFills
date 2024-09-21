c1fe0d4852b89f23f1047389fd1ac723cd8bfe3b
clasher3531 <nikhil.a.joshi@capgemini.com>
2024-09-21 13:59:10 +0530

    Update Reduce polyfills

--- Array/Reduce.js
+++ Array/Reduce.js
@@ -1,22 +1,31 @@
 // polyfill for reduce method
 
-Array.prototype.myReduce = function(callback, initialVal) {
-    if (typeof callback !== 'function') {
-        throw new Error('Type of callback must be a function');
-    }
+Array.prototype.myReduce = function () {
+  var args = arguments;
+  if (typeof args[0] !== "function") {
+    throw new Error("Type of callback must be a function");
+  }
 
-    if (!initialVal && this.length === 0) {
-        throw new Error('Initial Value must be there for an empty array');
-    }
-    var result = initialVal;
-    for (var i = 0; i < this.length; i++) {
-        result = result ? callback(result, this[i]) : this[0];
-    }
-    return result;
-}
+  if (!args[1] && this.length === 0) {
+    throw new Error("Initial Value must be there for an empty array");
+  }
+  var result = args.length === 1 ? "" : args[1];
+  for (var i = 0; i < this.length; i++) {
+    result = result !== "" ? args[0](result, this[i], i, this) : this[0];
+  }
+  return result;
+};
 
-var a = [1, 2, 3, 4];
-var b = a.myReduce(function(sum, item) {
-    return sum + item;
-}, 2);
-console.log(b);
+const arr = [1, 2, 3, 4, 5, 6].reverse();
+const reducer = (a, b, c, d) => {
+  if (Array.isArray(a)) {
+    a.push([b, c, d]);
+    return a;
+  } else {
+    return [b, c, d];
+  }
+};
+
+console.log(arr.myReduce(reducer, []));
+console.log(arr.reduce(reducer, []));
+console.log(arr.myReduce(reducer));
